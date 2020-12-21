2ba60cc4900a8e02c5cbb3c421db7ba3dcbe8bf2
roman <roman@romans-iMac.local>
2020-12-22 01:53:46 +0200

    fix intentional error

--- src/cli.js
+++ src/cli.js
@@ -2,4 +2,4 @@ import readlineSync from 'readline-sync';
 
 export const greeting = console.log('Welcome to the Brain Games!');
 export const name = readlineSync.question('May I have your name? ');
-export const sayHello = console.log (`Hello, ${name}!`);
+export const sayHello = console.log(`Hello, ${name}!`);
