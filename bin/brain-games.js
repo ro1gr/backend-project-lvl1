b2bde78146258451cb94ed8ddd32973df439601a
roman <roman@romans-iMac.local>
2020-12-21 18:23:33 +0200

    added greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import * as gameStart from '../src/cli.js';
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = console.log('Welcome to the Brain Games!');
+export const name = readlineSync.question('May I have your name? ');
+export const sayHello = console.log(`Hello, ${name}!`);
