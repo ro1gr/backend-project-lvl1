c4e0eb61dd20e1a59f4dc50e7a0593dae71ec157
roman <roman@romans-iMac.local>
2020-12-26 11:01:05 +0200

    fix eslint errors

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import {name} from './greetings.js';
+import { name } from './greetings.js';
 
 export const brainEven = () => {
   const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
   const randomNumber = () => Math.ceil(Math.random() * 13);
 
-  const isEven = (number) => number % 2 === 0 ? true : false;
+  const isEven = (number) => number % 2 === 0;
   const correctAnswer = (number) => isEven(number) ? 'yes' : 'no';
   const isCorrect = (answer, number) => answer === correctAnswer(number);
   let mistake = false;
@@ -16,7 +16,7 @@ export const brainEven = () => {
 
   for (let i = 0; i < 3 && mistake !== true; i += 1) {
     const number = randomNumber();
-    const question = 'Question: ' + number;
+    const question = `Question: ${number}`;
 
     console.log(question);
     const answer = readlineSync.question('Your answer: ');
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import {name} from '../bin/greetings.js';
-import {brainEven} from '../bin/brain-even.js';
+// import readlineSync from 'readline-sync';
+import { name } from '../bin/greetings.js';
+import { brainEven } from '../bin/brain-even.js';
 
 // export const greeting = console.log('Welcome to the Brain Games!');
 // export const name = readlineSync.question('May I have your name? ');
