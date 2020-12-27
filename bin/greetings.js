116bd17d3ce25a272c85892db362d3c17151d559
roman <roman@romans-iMac.local>
2020-12-27 02:41:10 +0200

    remove redundant module

--- bin/greetings.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-
-export const greeting = console.log('Welcome to the Brain Games!');
-export const name = readlineSync.question('May I have your name? ');
-export const sayHello = console.log(`Hello, ${name}!`);
