c176e66bbe0df28c7c3b0c66176d55fefc49bb8f
roman <roman@romans-iMac.local>
2020-12-27 02:45:41 +0200

    add brain-calc module

---
+++ bin/brain-calc.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
+
+export const brainCalc = () => {
+  const rules = 'What is the result of the expression?';
+  const randomNumber = () => Math.ceil(Math.random() * 13);
+  const operators = ['+', '-', '*'];
+  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+
+  const correctAnswer = (n1, n2, operator) => String(eval(`${n1} ${operator} ${n2}`));
+  const isCorrect = (answer, n1, n2, operator) => answer === correctAnswer(n1, n2, operator);
+  let mistake = false;
+
+  console.log(rules);
+
+  for (let i = 0; i < 3 && mistake !== true; i += 1) {
+    const number1 = randomNumber();
+    const number2 = randomNumber();
+    const question = `Question: ${number1} ${randomOperator} ${number2}`;
+
+    console.log(question);
+    const answer = readlineSync.question('Your answer: ');
+    if (isCorrect(answer, number1, number2, randomOperator)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer(number1, number2, randomOperator)}'.`);
+      mistake = true;
+    }
+  }
+
+  if (mistake) {
+    console.log(`Let's try again, ${name}!`);
+  } else {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+brainCalc();
