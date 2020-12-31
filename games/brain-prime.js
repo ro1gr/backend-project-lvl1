77c2dc88f0b3757a2132bb74c1a67d72a37146c0
roman <roman@romans-iMac.local>
2020-12-31 16:57:21 +0200

    fix brain-prime.js

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -1,9 +1,9 @@
-export default (n) => {
-  if (n === 0 || n === 1) {
+export default (number) => {
+  if (number === 0 || number === 1) {
     return 'no';
   }
-  for (let i = 2; i < n / 2; i += 1) {
-    if (n % i === 0) {
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
       return 'no';
     }
   }
